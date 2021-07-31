ff4efcf747a1cf1568513b9fe5159da6851e2378
Jason Curl <jcurl@arcor.de>
2021-07-31 19:30:45 +0200

    Datastructures: Move over to the .NET SDK project format
    Create a README.md file describing this project
    
    Issue: DOTNET-343, DOTNET-352

--- Datastructures/Properties/AssemblyInfo.cs
+++ Datastructures/Properties/AssemblyInfo.cs
@@ -1,18 +1,5 @@
 ﻿using System;
-using System.Reflection;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Core.Datastructures")]
-[assembly: AssemblyDescription("Collection of useful datastructures")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RJCP.Core.Datastructures")]
-[assembly: AssemblyCopyright("Copyright © Jason Curl, 2019")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -20,20 +7,3 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 [assembly: CLSCompliant(true)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("bbb453b0-5024-4ff6-a323-7c2d711e4936")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.8.0.0")]
-[assembly: AssemblyFileVersion("0.8.0.0")]
-[assembly: AssemblyInformationalVersion("0.8.0")]
---
+++ README.md
@@ -0,0 +1,4 @@
+# Datastructures
+
+This library contains a set of useful implementations that can be used in
+multiple different projects.
