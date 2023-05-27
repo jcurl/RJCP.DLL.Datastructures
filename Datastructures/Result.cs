2956bc3e098a625503d2f9bd4171a38af14a2553
Jason Curl <jcurl@arcor.de>
2023-05-27 15:07:36 +0200

    Result: Implement a Result<T> type
    Inspired by a YouTube video [1], and reference to DotNext [2], provide our own implementation that can be experimented with.
    
    Only a subset of functionality is implemented, it's not exactly the same, and can be used as is.
    
    [1]: https://www.youtube.com/watch?v=YbuSuSpzee4
    [2]: https://github.com/dotnet/dotNext/blob/master/src/DotNext/Result.cs
    
    Issue: DOTNET-788

---
+++ Datastructures/Result.cs
@@ -0,0 +1,242 @@
+﻿namespace RJCP.Core
+{
+    using System;
+    using System.Runtime.CompilerServices;
+
+#if NET45_OR_GREATER || NETSTANDARD
+    using System.Runtime.ExceptionServices;
+#endif
+
+    /// <summary>
+    /// Represents extension methods for type <see cref="Result{T}"/>.
+    /// </summary>
+    public static class Result
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="Result{T}"/> from the specified value.
+        /// </summary>
+        /// <typeparam name="T">The type of the value.</typeparam>
+        /// <param name="value">The value to be placed to the container.</param>
+        /// <returns>The value encapsulated by <see cref="Result{T}"/>.</returns>
+        public static Result<T> FromValue<T>(T value)
+        {
+            return new Result<T>(value);
+        }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="Result{T}"/> from the specified exception.
+        /// </summary>
+        /// <typeparam name="T">The type of the value.</typeparam>
+        /// <param name="e">The exception to be placed to the container.</param>
+        /// <returns>The exception encapsulated by <see cref="Result{T}"/>.</returns>
+        public static Result<T> FromException<T>(Exception e)
+        {
+            return new Result<T>(e);
+        }
+
+        /// <summary>
+        /// Gets a reference to the underlying value.
+        /// </summary>
+        /// <typeparam name="T">The type of the result.</typeparam>
+        /// <param name="result">The result container.</param>
+        /// <returns>The reference to the result.</returns>
+        /// <exception cref="Exception">The result is unavailable.</exception>
+        public static ref readonly T GetReference<T>(in Result<T> result)
+        {
+            return ref Result<T>.GetReference(in result);
+        }
+    }
+
+    /// <summary>
+    /// Represents a result of operation which can be actual result or exception.
+    /// </summary>
+    /// <typeparam name="T">The type of the value stored in the Result monad.</typeparam>
+    public readonly struct Result<T>
+    {
+        private readonly T m_Value;
+#if NET45_OR_GREATER || NETSTANDARD
+        private readonly ExceptionDispatchInfo m_Exception;
+#else
+        private readonly Exception m_Exception;
+#endif
+
+        /// <summary>
+        /// Initializes a new successful result.
+        /// </summary>
+        /// <param name="value">The value to be stored as result.</param>
+        public Result(T value)
+        {
+            m_Value = value;
+            m_Exception = null;
+        }
+
+        /// <summary>
+        /// Initializes a new unsuccessful result.
+        /// </summary>
+        /// <param name="error">The exception representing error. Cannot be <see langword="null"/>.</param>
+        public Result(Exception error)
+        {
+#if NET45_OR_GREATER || NETSTANDARD
+            if (error is null) throw new ArgumentNullException(nameof(error));
+            m_Exception = ExceptionDispatchInfo.Capture(error);
+#else
+            if (error == null) throw new ArgumentNullException(nameof(error));
+            m_Exception = error;
+#endif
+            m_Value = default;
+        }
+
+#if NET45_OR_GREATER || NETSTANDARD
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+#endif
+        internal static ref readonly T GetReference(in Result<T> result)
+        {
+            result.Validate();
+            return ref result.m_Value;
+        }
+
+        /// <summary>
+        /// Indicates that the result is successful.
+        /// </summary>
+        /// <value><see langword="true"/> if this result is successful; <see langword="false"/> if this result represents exception.</value>
+        public bool HasValue
+        {
+            get
+            {
+#if NET45_OR_GREATER || NETSTANDARD
+                return m_Exception is null;
+#else
+                return m_Exception == null;
+#endif
+            }
+        }
+
+        private void Validate()
+        {
+#if NET45_OR_GREATER || NETSTANDARD
+            if (m_Exception is object) m_Exception.Throw();
+#else
+            if (m_Exception != null) throw m_Exception;
+#endif
+        }
+
+        /// <summary>
+        /// Extracts the actual result.
+        /// </summary>
+        /// <exception cref="Exception">This result is not successful.</exception>
+        public T Value
+        {
+            get
+            {
+                Validate();
+                return m_Value;
+            }
+        }
+
+        /// <summary>
+        /// Attempts to extract value from container if it is present.
+        /// </summary>
+        /// <param name="value">Extracted value.</param>
+        /// <returns><see langword="true"/> if value is present; otherwise, <see langword="false"/>.</returns>
+        public bool TryGet(out T value)
+        {
+            value = m_Value;
+#if NET45_OR_GREATER || NETSTANDARD
+            return m_Exception is null;
+#else
+            return m_Exception == null;
+#endif
+        }
+
+        /// <summary>
+        /// Gets exception associated with this result.
+        /// </summary>
+        public Exception Error
+        {
+            get
+            {
+#if NET45_OR_GREATER || NETSTANDARD
+                return m_Exception?.SourceException;
+#else
+                return m_Exception;
+#endif
+            }
+        }
+
+        /// <summary>
+        /// Extracts actual result.
+        /// </summary>
+        /// <param name="result">The result object.</param>
+        public static explicit operator T(Result<T> result)
+        {
+            return result.Value;
+        }
+
+        /// <summary>
+        /// Converts value into the result.
+        /// </summary>
+        /// <param name="result">The result to be converted.</param>
+        /// <returns>The result representing <paramref name="result"/> value.</returns>
+        public static implicit operator Result<T>(T result)
+        {
+            return new Result<T>(result);
+        }
+
+        /// <summary>
+        /// Indicates that both results are successful.
+        /// </summary>
+        /// <param name="left">The first result to check.</param>
+        /// <param name="right">The second result to check.</param>
+        /// <returns><see langword="true"/> if both results are successful; otherwise, <see langword="false"/>.</returns>
+        public static bool operator &(in Result<T> left, in Result<T> right)
+        {
+#if NET45_OR_GREATER || NETSTANDARD
+            return left.m_Exception is null && right.m_Exception is null;
+#else
+            return left.m_Exception == null && right.m_Exception == null;
+#endif
+        }
+
+        /// <summary>
+        /// Indicates that the result is successful.
+        /// </summary>
+        /// <param name="result">The result to check.</param>
+        /// <returns><see langword="true"/> if this result is successful; <see langword="false"/> if this result represents exception.</returns>
+        public static bool operator true(in Result<T> result)
+        {
+#if NET45_OR_GREATER || NETSTANDARD
+            return result.m_Exception is null;
+#else
+            return result.m_Exception == null;
+#endif
+        }
+
+        /// <summary>
+        /// Indicates that the result represents error.
+        /// </summary>
+        /// <param name="result">The result to check.</param>
+        /// <returns><see langword="false"/> if this result is successful; <see langword="true"/> if this result represents exception.</returns>
+        public static bool operator false(in Result<T> result)
+        {
+            // Note, this appears not testable, as we don't have a short-circuit operator that we can use.
+#if NET45_OR_GREATER || NETSTANDARD
+            return result.m_Exception is object;
+#else
+            return result.m_Exception != null;
+#endif
+        }
+
+        /// <summary>
+        /// Returns textual representation of this object.
+        /// </summary>
+        /// <returns>The textual representation of this object.</returns>
+        public override string ToString()
+        {
+#if NET45_OR_GREATER || NETSTANDARD
+            return m_Exception?.SourceException.ToString() ?? m_Value?.ToString() ?? "<NULL>";
+#else
+            return m_Exception?.ToString() ?? m_Value?.ToString() ?? "<NULL>";
+#endif
+        }
+    }
+}
---
+++ DatastructuresTest/ResultTest.cs
@@ -0,0 +1,179 @@
+﻿namespace RJCP.Core
+{
+    using System;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class ResultTest
+    {
+        private static Result<int> ParseSuccess()
+        {
+            return 42;
+        }
+
+        private static Result<int> ParseError()
+        {
+            return Result.FromException<int>(new ArgumentException("Test argument error"));
+        }
+
+        [Test]
+        public void ImplicitSuccess()
+        {
+            var result = ParseSuccess();
+            Assert.That(result.Value, Is.EqualTo(42));
+            Assert.That(result.HasValue, Is.True);
+        }
+
+        [Test]
+        public void Success()
+        {
+            var result = new Result<int>(10);
+            Assert.That(result.Value, Is.EqualTo(10));
+            Assert.That(result.HasValue, Is.True);
+        }
+
+        [Test]
+        public void ExplicitSuccess()
+        {
+            var result = new Result<int>(10);
+            int value = (int)result;
+            Assert.That(value, Is.EqualTo(10));
+            Assert.That(result.HasValue, Is.True);
+        }
+
+        [Test]
+        public void SuccessFromValue()
+        {
+            var result = Result.FromValue(42);
+            Assert.That(result.Value, Is.EqualTo(42));
+            Assert.That(result.HasValue, Is.True);
+        }
+
+        [Test]
+        public void SuccessFromConstructor()
+        {
+            var result = new Result<int>(42);
+            Assert.That(result.Value, Is.EqualTo(42));
+            Assert.That(result.HasValue, Is.True);
+        }
+
+        [Test]
+        public void Exception()
+        {
+            var result = ParseError();
+            Console.WriteLine($"{result}");
+            Assert.That(result.Error, Is.TypeOf<ArgumentException>());
+            Console.WriteLine($"{result.Error.StackTrace}");
+            Assert.That(result.HasValue, Is.False);
+        }
+
+        [Test]
+        public void ExceptionFromConstructor()
+        {
+            var result = new Result<int>(new ArgumentException("Test argument error"));
+            Assert.That(result.Error, Is.TypeOf<ArgumentException>());
+            Assert.That(result.HasValue, Is.False);
+        }
+
+        [Test]
+        public void NullException()
+        {
+            Assert.That(() => {
+                _ = Result.FromException<int>(null);
+            }, Throws.TypeOf<ArgumentNullException>());
+        }
+
+        [Test]
+        public void SuccessTrue()
+        {
+            var result = ParseSuccess();
+            Assert.That(result.HasValue, Is.True);
+            if (result) {
+                Assert.Pass();
+            } else {
+                Assert.Fail();
+            }
+        }
+
+        [Test]
+        public void ExceptionFalse()
+        {
+            var result = ParseError();
+            Assert.That(result.HasValue, Is.False);
+            if (result) {
+                Assert.Fail();
+            } else {
+                Assert.Pass();
+            }
+        }
+
+        [Test]
+        public void ExceptionOnExplicitAccess()
+        {
+            var result = ParseError();
+            Assert.That(() => {
+                _ = (int)result;
+            }, Throws.TypeOf<ArgumentException>());
+        }
+
+        [Test]
+        public void ExceptionOnValueAccess()
+        {
+            var result = ParseError();
+            Assert.That(() => {
+                _ = result.Value;
+            }, Throws.TypeOf<ArgumentException>());
+        }
+
+        [Test]
+        public void TryGetSuccess()
+        {
+            var result = ParseSuccess();
+            Assert.That(result.TryGet(out int value), Is.True);
+            Assert.That(value, Is.EqualTo(42));
+        }
+
+        [Test]
+        public void TryGetError()
+        {
+            var result = ParseError();
+            Assert.That(result.TryGet(out int _), Is.False);
+        }
+
+        private static int GetRef(int value)
+        {
+            return value;
+        }
+
+        [Test]
+        public void GetReference()
+        {
+            var result = ParseSuccess();
+            int value = GetRef(Result.GetReference(result));
+            Assert.That(value, Is.EqualTo(42));
+        }
+
+        [Test]
+        public void CheckResultsSuccess()
+        {
+            var r1 = ParseSuccess();
+            var r2 = ParseSuccess();
+
+            Assert.That(r1 & r2, Is.True);
+            Assert.That(r2 & r1, Is.True);
+        }
+
+        [Test]
+        public void CheckResultsError()
+        {
+            var r1 = ParseSuccess();
+            var r2 = ParseError();
+            var r3 = ParseError();
+
+            Assert.That(r1 & r2, Is.False);
+            Assert.That(r2 & r1, Is.False);
+            Assert.That(r2 & r3, Is.False);
+            Assert.That(r3 & r2, Is.False);
+        }
+    }
+}
