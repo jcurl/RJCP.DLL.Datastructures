8f4b6dff3178930019ad71dc76f7c8980bdd2274
Jason Curl <jcurl@arcor.de>
2024-03-03 12:10:57 +0100

    Datastructures: Suppress IDE0230 for UTF-8 string literals
    The test cases use bytes, that just so happens to be the same as a string literal. But the test case should use the bytes, as that is the underlying interface.
    
    Issue: DOTNET-995

---
+++ DatastructuresTest/GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Style", "IDE0230:Use UTF-8 string literal", Justification = "Testing byte streams, not UTF8 strings")]
